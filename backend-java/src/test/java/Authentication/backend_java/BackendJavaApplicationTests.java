a0190f9b176db0d062cd9f78022bdd6520f3845a
abhishek <abhishek.kushwaha@boomerangs.ai.com>
2026-07-28 15:35:29 +0530

    ashura

---
+++ backend-java/src/test/java/Authentication/backend_java/BackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package Authentication.backend_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
